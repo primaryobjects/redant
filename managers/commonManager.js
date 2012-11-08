3f7736264262cd2042b4a5ef72ffdab98281a98e
Kory Becker <kbecker@primaryobjects.com>
2012-11-08 11:54:02 -0500

    Created manager method for reading post data and safe-parsing json.

--- managers/commonManager.js
+++ managers/commonManager.js
@@ -6,5 +6,41 @@
         });
 
         res.end(JSON.stringify({ error: message }));
+    },
+
+    getPostData: function (req, res, onData) {
+        var content = '';
+
+        req.on('data', function (data) {
+            if (content.length > 1e6) {
+                // Flood attack or faulty client, nuke request.
+                res.writeHead(413, {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                });
+
+                res.end(JSON.stringify({ error: 'Request entity too large.' }));
+            }
+
+            content += data;
+        });
+
+        req.on('end', function () {
+            // Return the posted data.
+            onData(content);
+        });
+    },
+
+    tryParseJson: function (data, onJson) {
+        try {
+            // Try parsing the JSON object.
+            json = JSON.parse(data);
+
+            onJson(json);
+        }
+        catch (err) {
+            // Invalid JSON.
+            onJson(null, { message: 'Invalid JSON object passed in query: ' + data + '. ' + err.message });
+        }
     }
 }
