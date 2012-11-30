ad34ba6abd7231a77eb9c7f4b1028d9f8e9ab62a
Kory Becker <kbecker@primaryobjects.com>
2012-11-30 11:38:06 -0500

    Updated appl to use mongoskin.

--- routes/appl/index.js
+++ routes/appl/index.js
@@ -1,50 +1,28 @@
-﻿var mongo = require('mongodb'),
-    config = require('../../config/config'),
-    managers = require('../../managers/commonManager');
-
+﻿var mongo = require('mongoskin'),
+    config = require('../../config/config');
 exports.getItemId = function (req, res) {
-    // Open connection.
-    mongo.connect(config.mongo.connectionString2, function (err, connection) {
-        // Get collection.
-        connection.collection('appl_7', function (err, collection) {
-            collection.findOne({ 'item_id': req.params.value }, function (err, item) {
-                // Verify we have a document.
-                if (!err && item != null) {
-                    res.send(JSON.stringify(item));
-                }
-                else {
-                    // No document found.
-                    res.writeHead(404, {
-                        "Content-Type": "application/json",
-                        "Access-Control-Allow-Origin": "*"
-                    });
-
-                    res.end(JSON.stringify({ error: 'ItemId not found.', id: req.params.value }));
-                }
-            });
-        });
+    // Get collection.
+    mongo.db(config.mongo.connectionString2).collection('appl_7').findOne({ 'item_id': req.params.value }, function (err, item) {
+        // Verify we have a document.
+        if (!err && item != null) {
+            res.json(item);
+        }
+        else {
+            // No document found.
+            res.json({ error: 'No records found. ItemId ' + req.params.value }, 404);
+        }
     });
 }
 
 exports.getCity = function (req, res) {
-    // Open connection.
-    mongo.connect(config.mongo.connectionString2, function (err, connection) {
-        // Get collection.
-        connection.collection('appl_7', function (err, collection) {
-            collection.find({ 'city': req.params.value }).toArray(function (err, items) {
-                if (!err && items != null) {
-                    res.send(JSON.stringify(items));
-                }
-                else {
-                    // No document found.
-                    res.writeHead(404, {
-                        "Content-Type": "application/json",
-                        "Access-Control-Allow-Origin": "*"
-                    });
-
-                    res.end(JSON.stringify({ error: 'City not found.', id: req.params.value }));
-                }
-            });
-        });
+    // Get collection.
+    mongo.db(config.mongo.connectionString2).collection('appl_7').find({ 'city': req.params.value }).toArray(function (err, items) {
+        if (!err && items != null) {
+            res.json(items);
+        }
+        else {
+            // No document found.
+            res.json({ error: 'City not found. City ' + req.params.value }, 404);
+        }
     });
 }
