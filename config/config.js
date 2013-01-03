7296ec9ce5167f99397a9c448472ed6f9f3b59d4
Kory Becker <kbecker@primaryobjects.com>
2013-01-03 11:02:28 -0500

    Update.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 ﻿var config = {};
 
 config.mongo = {};
-config.mongo.connectionString = 'mongodb://user:pass@ds037997.mongolab.com:37997/databasename';
+config.mongo.connectionString = 'mongodb://';
 config.mongo.connectionString2 = '';
 config.version = 1.1;
 
